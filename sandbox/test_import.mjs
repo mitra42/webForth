ead986c487ee709f7119750a9f107f3fd0f3e391
Mitra Ardron <mitra@mitra.biz>
2020-09-27 12:14:41 +1000

    Rename sandbox

---
+++ sandbox/test_import.mjs
@@ -0,0 +1,15 @@
+
+// Works as long as file is extension .mgs or package contains type: module
+import r1 from '../index.js';
+console.log('r1 is', r1); // r1 is Promise
+r1.then((r2) => {
+  console.log('r2 is', r2); //r2 is class Forth
+})
+
+/* WORKS - but uses require
+const foo = require('../index.js');
+
+foo.then((res) => {
+  console.log("Res is", res);
+})
+*/
