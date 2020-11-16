8d63c0358062751de6d0c8fd18b65600fe7c0ded
Mitra Ardron <mitra@mitra.biz>
2020-11-16 14:29:05 +1100

    Rename test_node_api.mjs to example_node_api.mjs and copy as template for test_dump.mjs

---
+++ sandbox/test_dump.mjs
@@ -0,0 +1,13 @@
+import { Forth, ForthNodeExtensions} from '../index.js';
+// Normally this would be: import Forth from 'webforth';
+
+const CELLL = 2;
+const MEM = 8;
+const EM = 0x2000 * CELLL; // default is 0x2000 * CELLL
+const foo = new Forth({CELLL, EM, MEM, extensions: ForthNodeExtensions});
+foo.compileForthInForth()
+  .then(() => console.log('===forthInForth compiled'))
+  //.then(() => foo.cleanupBootstrap()).then(() => console.log('===forthInForth cleaned up'))
+  //.then(() => foo.interpret("WARM"));
+  .then(() => foo.console()) // Interactive console
+  .then(() => console.log('\nconsole exited'));
